3366e82fbdce7abbe5238d0dfc9d77c1fc2fddd8
pablo179 <cm.pablo.prog@gmail.com>
2022-06-15 14:55:53 -0500

    using compile mode

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
+import { text } from "https://x.nest.land/deno-figlet@0.0.5/mod.js";
+import { styles } from "https://deno.land/x/ansi_styles@1.0.1/mod.ts";
 import axiod from "https://deno.land/x/axiod/mod.ts";
 
+const wordleTitle = await text("Wordle", "starwars");
 const MAX_TRIES = 6;
 let previous_answer: string[] = [];
 
@@ -20,7 +23,7 @@ const fetchRandomWord = async () => {
 let wordleWord = await fetchRandomWord();
 
 const getWord = async () => {
-  const word = await window.prompt("Introduce your word");
+  const word = await prompt("Introduce your word");
   if (!word) {
     return { error: "Please introduce a word", word: "" };
   }
@@ -59,6 +62,7 @@ const colorLetter = (letter: string, index: number) => {
 
 const printGrid = () => {
   console.clear();
+  console.log(styles.green.open, wordleTitle, styles.green.close);
   for (let i = 0; i < previous_answer.length; i++) {
     let row = "";
     [...previous_answer[i]].forEach((letter, index) =>
